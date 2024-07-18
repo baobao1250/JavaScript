ada61b808bdbd76124c237b314b37ab7f61b29c7
baobao@gmail.com <@Baobao999>
2024-07-18 15:13:13 +0700

    update

--- complete-javascript-course/10-Functions/starter/script.js
+++ complete-javascript-course/10-Functions/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Methor 1
+const handel1 = value1 => {
+  return (handel2 = value2 => {
+    console.log(`${value1} ${value2}`);
+  });
+};
+
+// Methor 2
+const handel2 = value1 => value2 => {
+  console.log(`${value1} ${value2}`);
+};
